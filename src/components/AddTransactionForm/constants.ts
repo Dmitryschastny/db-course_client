849c1cc61335854cdbd55352bf7283f99118c1b9
Dmitry Shchasny <dmitryschastny@gmail.com>
2020-05-15 22:22:06 +0300

    Add transaction

--- src/components/AddTransactionForm/constants.ts
+++ src/components/AddTransactionForm/constants.ts
@@ -1,23 +1,32 @@
 import { Languages } from 'services/LanguagesService';
 
 export interface StringEntries {
+  add: string;
   fields: {
     amount: string;
     transactionType: string;
+    account: string;
+    date: string;
   };
 }
 
 export const stringEntries: Languages<StringEntries> = {
   en: {
+    add: 'Add',
     fields: {
       amount: 'Amount',
       transactionType: 'Transaction type',
+      account: 'Account',
+      date: 'Date',
     },
   },
   ru: {
+    add: 'Добавить',
     fields: {
       amount: 'Количество',
       transactionType: 'Тип транзакции',
+      account: 'Аккаунт',
+      date: 'Дата',
     },
   },
 };
--- src/components/AddTransactionForm/index.tsx
+++ src/components/AddTransactionForm/index.tsx
@@ -7,12 +7,15 @@ import { useStrings } from 'hooks/useStrings';
 import { FormikSelect } from 'components/FormikSelect';
 import { clients } from 'services/clients.config';
 import { TransactionType } from 'services/TransactionTypesService';
+import { Account } from 'services/AccountsService';
+import DatePicker from 'react-datepicker';
 import { StringEntries, stringEntries } from './constants';
+import 'react-datepicker/dist/react-datepicker.css';
 
 interface FormikValues {
-  accountId: number;
-  typeId: number;
   amount: number;
+  typeId: number;
+  accountId: number;
   date: number;
   note?: string;
   categoryId?: number;
@@ -25,11 +28,13 @@ const AddTransactionForm: React.FC = () => {
   const [transactionTypes, setTransactionTypes] = useState<TransactionType[]>(
     []
   );
+  const [accounts, setAccounts] = useState<Account[]>([]);
 
   useEffect(() => {
     clients.transactionTypes
       .getAll()
       .then(({ data }) => setTransactionTypes(data));
+    clients.accounts.getAll().then(({ data }) => setAccounts(data));
   }, []);
 
   const strings = useStrings<StringEntries>(stringEntries);
@@ -39,7 +44,7 @@ const AddTransactionForm: React.FC = () => {
       accountId: 0,
       typeId: 0,
       amount: 0,
-      date: 0,
+      date: Date.now(),
     },
     onSubmit: async values => {
       console.log(values);
@@ -50,6 +55,12 @@ const AddTransactionForm: React.FC = () => {
   const transactionTypesOptions = transactionTypes.map(t => (
     <option key={t.id} value={t.id}>
       {t.name}
+    </option>
+  ));
+
+  const accountsOptions = accounts.map(a => (
+    <option key={a.id} value={a.id}>
+      {a.name}
     </option>
   ));
 
@@ -68,6 +79,30 @@ const AddTransactionForm: React.FC = () => {
             <FormikSelect label={strings.fields.transactionType} name="typeId">
               {transactionTypesOptions}
             </FormikSelect>
+
+            <FormikSelect label={strings.fields.account} name="accountId">
+              {accountsOptions}
+            </FormikSelect>
+
+            <div className="flex justify-between mb-6">
+              <label className="w-full mr-5" htmlFor="date">
+                {strings.fields.date}
+              </label>
+              <DatePicker
+                id="date"
+                selected={formik.values.date}
+                withPortal
+                onChange={date => {
+                  if (date) {
+                    formik.setFieldValue('date', +date);
+                  }
+                }}
+              />
+            </div>
+
+            <button className="mb-1 w-auto self-end" type="submit">
+              {strings.add}
+            </button>
           </>
         );
       }}
