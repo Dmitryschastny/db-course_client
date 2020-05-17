af98d9b83528efc9f74957a53dbf76f09b0b362a
Dmitry Shchasny <dmitryschastny@gmail.com>
2020-05-17 13:00:02 +0300

    Update transactions list

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { PageTemplate } from 'components/templates/PageTemplate';
 import { Modal } from 'components/Modal';
 import { AddTransactionForm } from 'components/AddTransactionForm';
 import { Transaction } from 'services/TransactionsService';
 import { clients } from 'services/clients.config';
+import { AppContext } from 'App';
 
 const Transactions: React.FC = () => {
+  const { settings } = useContext(AppContext);
+
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   const [loading, setLoading] = useState(true);
@@ -17,34 +20,61 @@ const Transactions: React.FC = () => {
     });
   }, []);
 
-  const transactionsRows = transactions.map(t => (
-    <>
-      {/* <tr>
-        <td colSpan={2}>
-          <div>April 29, Wednesday</div>
-        </td>
-      </tr> */}
-      <tr>
-        <td>
-          <div className="flex">
-            <div
-              className="flex flex-shrink-0 items-center 
+  let lastDate = transactions.length
+    ? new Date(transactions[0].date)
+    : new Date();
+  const transactionsRows = transactions.map((t, i) => {
+    const date = new Date(t.date);
+
+    const showDate =
+      date.getDate() < lastDate.getDate() ||
+      date.getMonth() < lastDate.getMonth() ||
+      date.getFullYear() < lastDate.getFullYear() ||
+      i === 0;
+
+    if (showDate) {
+      lastDate = date;
+    }
+
+    const formattedDate = date.toLocaleString(settings?.language.code || 'en', {
+      month: 'long',
+      year: 'numeric',
+      day: 'numeric',
+    });
+
+    return (
+      <>
+        {showDate && (
+          <tr>
+            <td className="pb-2" colSpan={2}>
+              <div className="border-b"> {showDate && formattedDate}</div>
+            </td>
+          </tr>
+        )}
+        <tr>
+          <td className="pb-2">
+            <div className="flex">
+              <div
+                className="flex flex-shrink-0 items-center 
                     justify-center rounded-full overflow-hidden h-12 w-12 border-solid border-2 border-black"
-            >
-              <i className="material-icons text-4xl">{t.category?.icon.name}</i>
-            </div>
-            <div className="flex justify-between px-4 w-full">
-              <div>
-                <div className="text-lg font-bold">{t.category?.name}</div>
-                <div className="text-gray-600">{t.type.name}</div>
+              >
+                <i className="material-icons text-4xl">
+                  {t.category?.icon.name}
+                </i>
               </div>
-              <div>{t.amount} BYN</div>
+              <div className="flex justify-between px-4 w-full">
+                <div>
+                  <div className="text-lg font-bold">{t.category?.name}</div>
+                  <div className="text-gray-600">{t.type.name}</div>
+                </div>
+                <div>{t.amount} BYN</div>
+              </div>
             </div>
-          </div>
-        </td>
-      </tr>
-    </>
-  ));
+          </td>
+        </tr>
+      </>
+    );
+  });
 
   const pageContent = (
     <>
